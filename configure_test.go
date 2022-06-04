edab8cea65fd062cb94f8cc9e242481ffab9532c
Carlo Lobrano <c.lobrano@gmail.com>
2022-06-04 15:29:11 +0200

    test: configure: clean up
    No need for:
    - t.Fail call if already using t.Fatal
    - testing NewConfiguration, since all it does more than Parse function
      is reading the file into a slice of bytes

--- configure_test.go
+++ configure_test.go
@@ -23,7 +23,7 @@ action:
     email: false
 `
 
-func TestConfigurationFromData(t *testing.T) {
+func TestConfiguration(t *testing.T) {
 	var config Configuration
 	err := config.Parse([]byte(data))
 	if err != nil {
@@ -31,66 +31,26 @@ func TestConfigurationFromData(t *testing.T) {
 	}
 	if config.Urls[0] != "websiteA.com" {
 		t.Fatalf("urls 0 is '%s'", config.Urls[0])
-		t.Fail()
 	}
 	if config.Urls[1] != "websiteB.com" {
 		t.Fatalf("urls 1 is '%s'", config.Urls[1])
-		t.Fail()
 	}
 	if config.Timeout.Value != 1*time.Second {
 		t.Fatalf("timeout is %+v", config.Timeout)
-		t.Fail()
 	}
 	if config.Schedule.Hour != 1 {
 		t.Fatalf("schedule hour is %d", config.Schedule.Hour)
-		t.Fail()
 	}
 	if config.Schedule.Minute != 2 {
 		t.Fatalf("schedule minute is %d", config.Schedule.Minute)
-		t.Fail()
 	}
 	if config.Schedule.Second != 3 {
 		t.Fatalf("schedule second is %d", config.Schedule.Second)
-		t.Fail()
 	}
 	if config.Action.On_success.Telegram != true {
 		t.Fatalf("action on-success service Telegram is '%v'", config.Action.On_success.Telegram)
-		t.Fail()
 	}
 	if config.Action.On_success.Email != false {
 		t.Fatalf("action on-success service Email is '%v'", config.Action.On_success.Telegram)
-		t.Fail()
-	}
-}
-
-func TestConfigurationFromFileName(t *testing.T) {
-	config, err := NewConfiguration("configure_example.yml")
-	if err != nil {
-		t.Fatalf("%v", err)
-		t.Fail()
-	}
-	if (*config).Urls[0] != "www.google.com" {
-		t.Fatalf("url 0 is not www.google.com")
-		t.Fail()
-	}
-	if (*config).Urls[1] != "www.duckduckgo.com" {
-		t.Fatalf("url 1 is not www.duckduckgo.com")
-		t.Fail()
-	}
-	if (*config).Schedule.Hour != 1 {
-		t.Fatalf("schedule Hour is %d", (*config).Schedule.Hour)
-		t.Fail()
-	}
-	if (*config).Schedule.Minute != 2 {
-		t.Fatalf("schedule Minute is %d", (*config).Schedule.Minute)
-		t.Fail()
-	}
-	if (*config).Schedule.Second != 3 {
-		t.Fatalf("schedule Second is %d", (*config).Schedule.Second)
-		t.Fail()
-	}
-	if (*config).Action.On_success.Telegram != true {
-		t.Fatalf("Action OnSuccess Telegram is %v", (*config).Action.On_success.Telegram)
-		t.Fail()
 	}
 }
